df6d2da652a82b37fd0450c0427cca64ecbe0c30
Eylam65 <tandrigoal@gmail.com>
2025-05-18 21:26:41 +0700

    Upload frontend
    Bisa follow
    Bisa add event

---
+++ code/frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Dashboard from "@/components/dashboard"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen bg-gray-950">
+      <Dashboard />
+    </main>
+  )
+}
