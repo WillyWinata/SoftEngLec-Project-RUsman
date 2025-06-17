f50cace568684b1c7419b2b6f89dac39f6dcca03
Willsen Wiratan <willsenwork247@gmail.com>
2025-06-17 10:08:57 +0700

    Fix: Container
    - Fix database and containerization so both database and backend can both connect.

--- code/backend/infrastructure/database/connection.go
+++ code/backend/infrastructure/database/connection.go
@@ -1,8 +1,10 @@
 package database
 
 import (
+	"log"
 	"os"
 	"sync"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -14,10 +16,6 @@ var (
 	err  error
 )
 
-func init() {
-	// _ = godotenv.Load("config/.env")
-}
-
 func GetDB() *gorm.DB {
 	once.Do(func() {
 		host := os.Getenv("DB_HOST")
@@ -27,9 +25,14 @@ func GetDB() *gorm.DB {
 		name := os.Getenv("DB_NAME")
 
 		dsn := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + name + "?charset=utf8mb4&parseTime=True&loc=Local"
-		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-		if err != nil {
-			panic("failed to connect to database: " + err.Error())
+
+		for {
+			db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+			if err == nil {
+				break
+			}
+			log.Println("Waiting for database to be ready...")
+			time.Sleep(2 * time.Second)
 		}
 	})
 
